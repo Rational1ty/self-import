aa4acff009e5645c8a613a34db3a6b5f5cd9c2e1
Matthew Davidson <69492798+Rational1ty@users.noreply.github.com>
2021-01-17 10:32:49 -0600

    Delete bin directory

--- bin/install.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const args = process.argv.slice(2);
-const s = args[0].split('/');
-console.log(`Installing ${process.argv.slice(2)[0]} ...`);
